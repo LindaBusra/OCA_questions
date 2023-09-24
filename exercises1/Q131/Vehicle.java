e6023e1843a3f95353b82d3caf83a4eec46acf09
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-09-24 14:06:15 +0200

    Q131

---
+++ exercises1/Q131/Vehicle.java
@@ -0,0 +1,14 @@
+package Others.OCA.exercises1.Q131;
+
+public class Vehicle {
+
+    int x;
+
+    Vehicle() {
+        this(10);       // line n1
+    }
+
+    Vehicle(int x) {
+        this.x = x;
+    }
+}
