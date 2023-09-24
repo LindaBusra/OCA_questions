b1ad7382ec0766722dbb8ae5ee736e1a97caf822
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-09-24 14:06:00 +0200

    Q131

---
+++ exercises1/Q131/Car.java
@@ -0,0 +1,34 @@
+package Others.OCA.exercises1.Q131;
+
+public class Car extends Vehicle {
+
+    int y;
+
+    Car() {
+        super();
+//        this(20);   // line n2
+    }
+
+    Car(int y) {
+        this.y = y;
+    }
+
+    public String toString() {
+        return super.x + ":" + this.y;
+    }
+
+    public static void main (String[] args) {
+       Vehicle y = new Car();
+       System.out.println(y);
+    }
+
+}
+
+/*
+What is the result?
+A. 10:20
+B. 0:20
+C. Compilation fails at line n1
+D. Compilation fails at line n2  +
+Answer: D
+ */
