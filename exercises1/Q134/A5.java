aa016349c84951251e6ed61a37f57a537dea4bef
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-09-25 21:31:18 +0200

    Q134

---
+++ exercises1/Q134/A5.java
@@ -0,0 +1,33 @@
+package Others.OCA.exercises1.Q134;
+
+   abstract class A5 {        //it was  final abstract class A5, class can be final or abstract, not both of them
+
+       protected static  int i;
+    void doStuff(){
+    }
+
+    abstract void doIt();
+}
+
+
+/*
+
+134. Which two class definitions fail to compile? (Choose two.)
+
+A. Option A
+B. Option B
+C. Option C   +
+D. Option D
+E. Option E   +
+
+ */
+
+
+/*
+note:
+
+class A4{
+				protected static final int i;   //iwe have to initialize it
+				private A4() { 	}
+			}
+ */
