5dcac1882fd6840b6d073a31102037df7f68db1c
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-09-29 22:17:37 +0200

    Q140

---
+++ exercises1/Q140/Q140.java
@@ -0,0 +1,31 @@
+package Others.OCA.exercises1.Q140;
+public class Q140 {
+    public static void main(String[] args) {
+        int iVar = 100;
+        float fVar = 100.100f;
+        double dVar = 123;
+//        fVar = iVar;    //line 7
+//        iVar = fVar;    //line 8
+//	    fVar = dVar;    //line 9
+//        dVar = fVar;    //line 10
+//        iVar = dVar;    //line 11
+//	    dVar = iVar;    //line 12
+
+
+        //boolean   -char-    byte      -short      -int        -long   -float  -double
+        //1 bit     2 byte    1 byte    2 byte      4 byte      8 byte  4 byte  8 byte
+
+	        /* which three lines fail to compile? (choose three.)
+	        A. Line 7
+	        B. Line 8  +
+	        C. Line 9  +
+	        D. Line 10
+	        E. Line 11  +
+	        F. Line 12
+
+	        Answer: BCE
+	         */
+
+
+    }
+}
