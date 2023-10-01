af3ed5ddddbca147c397ac8a55aaa697c68d962f
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-01 23:14:23 +0200

    Q144

---
+++ exercises1/Q144/Greeting.java
@@ -0,0 +1,17 @@
+package Others.OCA.exercises1.Q144;
+
+public class Greeting {
+
+    public static void main(String[]args) {
+        System.out.println("Hello " + args[0]);
+
+
+    }
+}
+
+
+//
+//Which set of commands prints Hello Duke in the console?
+//
+//		Answer: javac Greeting.java
+//		        java Greeting Duke
