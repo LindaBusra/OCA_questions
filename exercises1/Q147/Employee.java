c03643cf653ef19b730ff90eb1f5751848de57f0
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-02 17:57:33 +0200

    Q147

---
+++ exercises1/Q147/Employee.java
@@ -0,0 +1,54 @@
+package Others.OCA.exercises1.Q147;
+
+public class Employee {
+    private String name;
+    private int age;
+    private int salary;
+
+//  public Employee(){
+
+    //    }
+    public Employee(String name, int age) {
+        setName(name);
+        setAge(age);
+        setSalary(2000);
+    }
+
+    public Employee(String name, int age, int salary) {
+        this(name, age);
+        setSalary(salary);
+    }
+
+
+    //getter and setter methods for attributes go here
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void printDetails(){
+        System.out.println(name+ " : "+ age + " : "+ salary);
+    }
+}
+
+
+class Test{
+    public static void main(String[] args) {
+//        Employee e1 = new Employee();  //we need to create default constructor
+        Employee e2 = new Employee("Jack", 50);
+        Employee e3 = new Employee("Chloe", 40, 5000);
+
+//        e1.printDetails();
+        e2.printDetails();
+        e3.printDetails();
+    }
+
+}
+
+//Answer:
+//B compilation fails in the Test class
