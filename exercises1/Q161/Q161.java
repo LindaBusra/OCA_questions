c412db7728efba197c2a533e1d775a8f8aa90f90
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-09 16:40:09 +0200

    Q161

---
+++ exercises1/Q161/Q161.java
@@ -0,0 +1,30 @@
+package Others.OCA.exercises1.Q161;
+
+public class Q161 {
+
+
+
+    /*
+161. Which two initialization statements are valid? (Choose two.)
+A. Boolean available = “TRUE”:
+B. String tmpAuthor = author, author =”Mc Donald”;
+C. Double price = 200D;   +
+D. Integer pages = 20;    +
+
+     */
+
+
+//    Boolean available = “TRUE”:
+//    String tmpAuthor = author, author =”Mc Donald”;
+    Double price = 200D;
+    Integer pages = 20;
+
+
+    public static void main(String[] args) {
+        String author ="Mc Donald";
+        String tmpAuthor = author;
+    }
+
+
+
+}
