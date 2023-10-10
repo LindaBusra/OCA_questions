e3dcd7627ab8fb3254fdb946b031d8f579d80820
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-10 22:22:32 +0200

    Q166

---
+++ exercises1/Q166/Q166.java
@@ -0,0 +1,16 @@
+package Others.OCA.exercises1.Q166;
+
+public class Q166 {
+
+    /*
+
+166. Which two statements are true about Java byte code? (Choose two.)
+
+A. It can be serialized across network.
+B. It can run on any platform that has a Java compiler.
+C. It can run on any platform.
+D. It has “.java” extension.
+E. It can run on any platform that has the Java Runtime Environment.
+Answer: A E
+     */
+}
