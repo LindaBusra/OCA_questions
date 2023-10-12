6bae25304d9ca51d0116d61e6de919e804d81d94
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-12 22:23:34 +0200

    Q172

---
+++ exercises1/Q172/Q172.java
@@ -0,0 +1,26 @@
+package Others.OCA.exercises1.Q172;
+
+public class Q172 {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        do {
+            System.out.print(num-- +" ");
+        }while (num==0);
+
+    }
+}
+
+
+/*
+
+What is the result?
+A. 5 4 3 2 1 0
+B. 5 4 3 2 1
+C. 4 2 1
+D. 5
+E. Nothing is printed
+Answer: D
+
+ */
