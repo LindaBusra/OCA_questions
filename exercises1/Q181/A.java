6d7a2d94ecbae2887eed6b4e998061dac4463e34
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-17 20:53:18 +0200

    Q181

---
+++ exercises1/Q181/A.java
@@ -0,0 +1,20 @@
+package Others.OCA.exercises1.Q181;
+
+public class A {
+
+    public void a(){
+        int a;
+    }
+}
+
+
+/*
+Which statement is true?
+A. Only the A.Java file compiles successfully.  +
+B. Only the B.java file compiles successfully.
+C. Only the C.java file compiles successfully.
+D. The A.Java and B.java files compile successfully.
+E. The B.java and C.java files compile successfully.
+F. The A.Java and C.java files compile successfully.
+Answer: A
+ */
