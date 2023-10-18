956be65573ae57030330e2a0cd65d6a8344a74f4
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-18 23:16:54 +0200

    Q184

---
+++ exercises1/Q184/Rectangle.java
@@ -0,0 +1,39 @@
+package Others.OCA.exercises1.Q184;
+
+public class Rectangle {
+    private double length;
+    private double heigth;
+    private double area;
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+
+    public void setHeigth(double heigth) {
+        this.heigth = heigth;        setArea();
+    }
+
+    private void setArea() {   //it was public
+        area = length * heigth;
+    }
+
+    public static void main(String[] args) {
+        Rectangle r = new Rectangle();
+        r.setLength(10);
+        r.setHeigth(20);
+        System.out.println(r.area);
+    }
+}
+
+/*
+Which two changes would encapsulate this class and ensure that the area field is always equal to length * height whenever the Rectangle class is used?
+A.	Call the setArea method at the end of the setHeight method.  +
+B.	Call the setArea method at the beginning of the setHeight method.
+C.	Call the setArea method at the end of the setLength method.
+D.	Call the setArea method at the beginning of the setLength method.
+E.	Change the setArea method to private.  +
+F.	Change the area field to public.
+Answer: AE
+
+ */
