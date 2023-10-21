7522090ddf854f13877c210b2eb2eda270d6bd01
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-21 21:39:58 +0200

    Q187

---
+++ exercises1/Q187/Test.java
@@ -0,0 +1,22 @@
+package Others.OCA.exercises1.Q187;
+
+public class Test {
+
+    public static void main(String[] args) {
+        if (args[0].equals("Hello") ? true : false){
+            System.out.println("Success");
+        }else {
+            System.out.println("Failure");
+        }
+    }
+
+
+}
+
+
+// And given the commands :
+// javac Test.java
+// Java Test Hello
+
+// What is the result?
+//Success
