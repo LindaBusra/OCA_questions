202686874eb222f802811136ce60b46ab2eff45f
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-25 19:14:41 +0200

    Q196

---
+++ exercises1/Q196/Q196.java
@@ -0,0 +1,27 @@
+package Others.OCA.exercises1.Q196;
+
+public class Q196 {
+
+
+/*
+
+196. Given this segment of code:
+
+    ArrayList<Cycle> myList = new ArrayList<>();
+    myList.add(new MotorCycle());
+
+
+Which two statements, if either were true, would make the code compile? (Choose two.) A.
+MotorCycle is an interface that implements the Cycle class.
+B. Cycle is an interface that is implemented by the MotorCycle class.
+C. Cycle is an abstract superclass of MotorCycle.
+D. Cycle and MotorCycle both extend the Transportation superclass.
+E. Cycle and MotorCycle both implement the Transportation interface.
+F. MotorCycle is a superclass of Cycle.
+Answer: B C
+
+
+
+*/
+
+}
