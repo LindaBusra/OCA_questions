53f2ca02fbd42e5941c9f18f2f3712759004e8ee
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-25 19:15:32 +0200

    Q198

---
+++ exercises1/Q198/market/USMarket.java
@@ -0,0 +1,18 @@
+package Others.OCA.exercises1.Q198.market;
+
+//insert code here
+import Others.OCA.exercises1.Q198.sales.SalesMan;
+import Others.OCA.exercises1.Q198.sales.products.Product;
+
+public class USMarket {
+
+    SalesMan sm;
+    Product p;
+}
+
+
+/*
+Which code fragment, when inserted at line 3, enables the code to compile?
+import sales.*;
+import sales.products.*;
+ */
