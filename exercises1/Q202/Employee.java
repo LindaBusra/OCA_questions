050e67af1ca0fc49d2fd36958e0c30acd052b554
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-27 21:37:56 +0200

    Q202

---
+++ exercises1/Q202/Employee.java
@@ -0,0 +1,31 @@
+package Others.OCA.exercises1.Q202;
+
+
+
+public class Employee {
+    public int salary;
+
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee();
+        Employee manager = new Manager();
+        Employee director = new Director();
+
+
+
+        //line n1
+
+    }
+}
+
+/*
+Which two options compile when placed at line n1 of the main method? (Choose two.)
+A. director.stockOptions = 1_000;
+B. employee.salary = 50_000;   +
+C. manager.budget = 1_000_000;
+D. manager.stockOption = 500;
+E. employee.budget = 200_000;  +
+F. director.salary = 80_000;  +
+Answer: B F
+ */
