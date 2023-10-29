cc55de2f0b4292cc3f9ece33694ae7c2528430e5
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-10-29 16:50:35 +0100

    Q208

---
+++ exercises1/Q208/Caller.java
@@ -0,0 +1,16 @@
+package Others.OCA.exercises1.Q208;
+
+class Caller {
+
+    private void init(){
+        System.out.println("Initialized");
+    }
+
+    private void start(){
+        init();
+        System.out.println("Started");
+    }
+}
+
+
+
