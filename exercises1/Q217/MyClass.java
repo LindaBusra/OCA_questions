841e7500b9cd1b88c4d070c8fb7ade392f36d771
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-03 19:19:53 +0100

    Q217

---
+++ exercises1/Q217/MyClass.java
@@ -0,0 +1,21 @@
+package Others.OCA.exercises1.Q217;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        String s = "Java SE 8 1";
+        int len = s.trim().length();
+        System.out.println(len);
+    }
+}
+
+/*
+What is the result?
+A. Compilation fails.
+B. 11  +
+C. 8
+D. 9
+E. 10
+Answer: B
+ */
