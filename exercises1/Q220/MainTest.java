58c6b464dc037694065d2d1fa5ae33c15a38919d
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-04 20:07:52 +0100

    Q220

---
+++ exercises1/Q220/MainTest.java
@@ -0,0 +1,24 @@
+package Others.OCA.exercises1.Q220;
+
+public class MainTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("String main " + args[0]);
+    }
+}
+
+//Output: String main 1 2 3
+
+/*
+and commands
+javac MainTest.java
+java MainTest "1 2 3"
+
+
+What is the result?
+A. String main 1
+B. An exception is thrown at runtime
+C. String main 1 2 3  +
+D. String main 123
+ */
