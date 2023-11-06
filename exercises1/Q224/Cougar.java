eb7e9949852998ba837eba8b35b15b0ed3979306
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-06 22:27:04 +0100

    Q224

---
+++ exercises1/Q224/Cougar.java
@@ -0,0 +1,13 @@
+package Others.OCA.exercises1.Q224;
+
+public class Cougar extends Puma {
+
+    public static void main(String[] args) {
+
+//        Puma puma = new Puma();  // line 7,  'Puma' is abstract; cannot be instantiated
+//        System.out.println(puma.getTailLength());
+    }
+
+    public int getTailLength(int length) {return 2;}
+ }
+
