d13924572fe32125c7164d5093681a89e3ca3db9
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-07 22:42:31 +0100

    Q226

---
+++ exercises1/Q226/Reindeer.java
@@ -0,0 +1,13 @@
+package Others.OCA.exercises1.Q226;
+
+public class Reindeer extends  Deer{
+
+    public Reindeer(int age) {
+        System.out.print("Reindeer");
+    }
+
+
+    public boolean hasHorns() {
+        return true;
+    }
+}
