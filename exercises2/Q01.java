2dc1360906043b66b42870d36f5ab6c42bb14090
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-08 20:21:55 +0100

    Q001

---
+++ exercises2/Q01.java
@@ -0,0 +1,22 @@
+package Others.OCA.exercises2;
+
+public class Q01 {
+    public static void main(String[] args) {
+
+        int num1=9;
+
+        int num2= num1++;    //num2=9, num1=10
+
+
+
+        if(num2 < 10){
+            System.out.println(num2 + " Hello World");
+        } else {
+            System.out.println(num1 + " Hello Universel");
+        }
+
+
+    }
+    }
+
+    //what is the result
