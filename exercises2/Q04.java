a1c0e42c9d6d7c39841ea2207f7acaee1aabc539
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-10 17:13:59 +0100

    Q004

---
+++ exercises2/Q04.java
@@ -0,0 +1,22 @@
+package Others.OCA.exercises2;
+
+public class Q04 {
+    public static void main(String[] args) {
+
+        String str1= "Java";
+        String str2=new String("java");
+
+        // line 10
+        if(str2.equals(str1.toLowerCase()))
+        //
+
+        {
+            System.out.println("Equal");
+        }else{
+            System.out.println("Not Equal");
+
+        }
+
+
+    }
+}
