a7df258764d550675b370017d24ba0c99395f20a
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-10 17:14:38 +0100

    Q006

---
+++ exercises2/Q06.java
@@ -0,0 +1,32 @@
+package Others.OCA.exercises2;
+
+public class Q06 {
+
+    public static void main(String[] args) {
+
+        long x = 10;
+//        int y = 2*x;
+
+        //To fix it we can make these changes:
+//        short x = 10;
+//        int y = 2 *(int) x;
+//        int y = (int) (2 * x);
+//        long y = 2 * x;
+
+
+      //notes
+      //byte-short-int-long-float-double          numeric data types
+      //auto widening
+      byte num1 = 15;
+      short num2 = num1;
+      long num3 = 1500;
+      double num4 = num3;
+
+
+      int num5 = 65;
+      short num6 = (short) num5;        //casting
+
+
+
+    }
+}
