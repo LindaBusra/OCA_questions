7653374d851232d8ffe7a39d94ae98cce99dd21f
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-11 12:00:15 +0100

    Q09

---
+++ exercises2/Q09.java
@@ -0,0 +1,19 @@
+package Others.OCA.exercises2;
+
+public class Q09 {
+
+    public static void main(String[] args) {
+
+        String s = "purr";
+        s.toUpperCase();
+        s.trim();
+        s.substring(1, 3);
+        s += " two";
+        System.out.println(s.length());
+
+        /*
+        what is the result?  8
+
+        */
+    }
+}
