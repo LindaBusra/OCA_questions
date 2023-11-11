465a1c6b9670fdc553bceba3cdd66c516512363f
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-11 11:59:27 +0100

    Q07

---
+++ exercises2/Q07.java
@@ -0,0 +1,15 @@
+package Others.OCA.exercises2;
+public class Q07 {
+
+    public static void main(String[] args) {
+
+        byte a = 40, b=50;
+//        byte sum = (byte) a + b;
+//        byte sum =(byte) a + (byte) b;
+
+        int sum1 = (byte) a + (byte) b;
+        byte sum = (byte) (a+b);
+        System.out.println(sum);
+
+    }
+}
