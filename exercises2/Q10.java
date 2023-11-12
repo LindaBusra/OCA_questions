9aacafe72ca8f37c2cb365e0515e86133dad2fa7
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-12 15:30:56 +0100

    Q10

---
+++ exercises2/Q10.java
@@ -0,0 +1,17 @@
+package Others.OCA.exercises2;
+
+public class Q10 {
+
+
+    //Which methods work properly?
+
+    public void methodA() { return;}
+//    public void methodB() { return null;}
+    public void methodD() {}
+
+
+    public int methodE() { return 9;}
+//    public int methodF() { return 9.0;}
+//    public int methodG() { return;}
+//    public int methodH() { return null;}
+}
