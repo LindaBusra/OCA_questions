b4fe78bc36aac3516a239636673c5f487c35f733
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-13 21:24:50 +0100

    Q14

---
+++ exercises2/Q14.java
@@ -0,0 +1,18 @@
+package Others.OCA.exercises2;
+
+public class Q14 {
+    public static void main(String[] args) {
+
+        int x = 1, y = 15;
+
+
+//        while x < 10    //without () while does not work
+        y--;
+        x++;
+        System.out.println(x+", "+y);
+
+        /*
+        Because of line 9 this code gives compile time error
+         */
+    }
+}
