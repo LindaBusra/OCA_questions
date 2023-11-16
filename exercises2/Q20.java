59d3729d2e6a35017c34d208011e0311b288eac2
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-16 23:19:26 +0100

    Q20

---
+++ exercises2/Q20.java
@@ -0,0 +1,15 @@
+package Others.OCA.exercises2;
+
+public class Q20 {
+    public static void main(String[] args) {
+
+
+        int arr[]={1,2,3,4};
+        System.out.println(arr instanceof Object);
+    }
+}
+
+//what is the output? //true
+
+//instance keyword is used to check if an object is of a certain type
+//instance of anahtar sozcugu, bir nesnenin belirli bir turde olup olmadigini kontrol etmek icin kullanilir
