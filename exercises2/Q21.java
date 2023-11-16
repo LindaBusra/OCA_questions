a1935e86bba460db55094a7f03645e1bc630a431
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-16 23:19:45 +0100

    Q21

---
+++ exercises2/Q21.java
@@ -0,0 +1,28 @@
+package Others.OCA.exercises2;
+
+public class Q21 {
+    public static void main(String[] args) {
+
+        char[]c = new char[2];
+        // INSERT CODE HERE
+        int length = c.length;
+        System.out.println(length);
+
+
+//                int length1 = c.capacity;
+//                int length2 = c.capacity();
+//                int length3 = c.length;       //+
+//                int length4 = c.length();
+//                int length5 = c.size;
+//                int length6 = c.size();
+
+        /*Which
+        A. int length = c.capacity;
+        B. int length = c.capacity();
+        C. int length = c.length;   + it works
+        D. int length = c.length();
+        E. int length = c.size;
+        F. int length = c.size();
+        G. None of the above. */
+    }
+}
