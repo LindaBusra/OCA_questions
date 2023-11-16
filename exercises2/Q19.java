992983923f21e1047629bea2d56b8293d2470a06
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-16 23:18:38 +0100

    Q19

---
+++ exercises2/Q19.java
@@ -0,0 +1,30 @@
+package Others.OCA.exercises2;
+
+import java.util.Arrays;
+
+public class Q19 {
+
+    public static void main(String[] args) {
+
+        //Which are true?
+
+        int[][] scores = new int[5][];
+        System.out.println(Arrays.deepToString(scores));        //[null, null, null, null, null]
+
+        Object[][][] cubbies = new Object[3][0][5];
+        System.out.println(Arrays.deepToString(cubbies));       //[[], [], []]
+//        String beans = new beans[6];      //it must be String beans[] = new String[6];
+//        int[][] types = new int[];          //it must be int[][] types = new int[5][];
+//        int[][] java = new int[][];        //it must be         int[][] java = new int[2][];
+
+
+    int arr[] = {1,2,3,4};
+    String arr1[] = new String[10];
+    int arr3[] = new int[] {2,3,4,5};
+
+
+
+
+
+    }
+}
