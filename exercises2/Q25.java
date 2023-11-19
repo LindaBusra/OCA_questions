dcf76d94bf6efe80b3dfd21f5aebdd9fc9a9bfd2
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-19 23:42:11 +0100

    Q25

---
+++ exercises2/Q25.java
@@ -0,0 +1,22 @@
+package Others.OCA.exercises2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q25 {
+    public static void main(String[] args) {
+
+        List<String> list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+//        list.add(7);
+        for (int i = 0; i < list.size(); i++)
+            System.out.print(list.get(i));
+
+
+
+        /*
+        CTE in line 12
+         */
+    }
+}
