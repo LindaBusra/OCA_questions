5582270387feaa7aa7b2a8eac4409b5c47fd3e87
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-21 21:34:01 +0100

    Q31

---
+++ exercises2/Q31.java
@@ -0,0 +1,20 @@
+package Others.OCA.exercises2;
+
+//What is the result of the following class?
+
+public class Q31 {
+
+
+    private static int $;
+
+    public static void main(String[] main) {
+    String a_b;
+    System.out.print($);
+//    System.out.print(a_b);
+
+    }
+}
+
+/*
+Compiler error on line 8.
+ */
