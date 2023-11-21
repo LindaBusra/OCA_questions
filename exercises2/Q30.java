e3290aab0818ce75b88c6ff25799cac7b2eae620
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-21 21:33:39 +0100

    Q30

---
+++ exercises2/Q30.java
@@ -0,0 +1,14 @@
+package Others.OCA.exercises2;
+public class Q30 {
+        private static int $;
+
+    public static void main(String[] args) {
+
+        String a_b;
+        System.out.println($);
+//        System.out.println(a_b);
+    }
+}
+
+
+//CTE in line 9
