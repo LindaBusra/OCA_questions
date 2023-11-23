f0b94e84ec6fe7ed2f5711ceb3d6b38492979344
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-23 23:28:00 +0100

    Q39

---
+++ exercises2/Q39.java
@@ -0,0 +1,51 @@
+package Others.OCA.exercises2;
+
+public class Q39 {
+
+    public static void main(String[] args) {
+
+
+
+        int i=1;
+
+        do{
+
+            if(i==3) {
+                continue;
+            } else if(i==5) {
+                System.out.print(i + " ");
+                break;
+            } else {
+                System.out.println(i + " ");
+            }
+            System.out.println("write");
+            i++;
+        } while(true);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
