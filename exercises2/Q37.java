203da22efd8c4112faee77609ef71951becae877
Linda Busra Berg <linda.busra.berg@gmail.com>
2023-11-23 23:26:51 +0100

    Q37

---
+++ exercises2/Q37.java
@@ -0,0 +1,49 @@
+package Others.OCA.exercises2;
+
+public class Q37 {
+
+
+    public static void main(String[] args) {
+
+
+        //Question 1: What will be the Output of the below code:
+        System.out.println('j' + 'a' + 'v' + 'a');     //a number from ascii codes
+
+
+        //Question
+        Integer num1 = 100;
+        Integer num2 = 100;
+        Integer num3 = 500;
+        Integer num4 = 500;
+
+        if(num1==num2){
+            System.out.println("num1 == num2");
+        }
+        else{
+            System.out.println("num1 != num2");
+        }
+        if(num3 == num4){
+            System.out.println("num3 == num4");
+        }
+        else{
+            System.out.println("num3 != num4");
+        }
+
+
+
+        //Question 2: What will be the Output of the below code:
+
+        for(int i = 0; true; i++) {
+            System.out.println("Hello");
+            break;
+        }
+
+
+
+
+
+
+
+
+    }
+}
